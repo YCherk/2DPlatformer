debb5d9b6eb066ab849caa99a5ed811edb30cdbd
YCherk <117645287+YCherk@users.noreply.github.com>
2023-10-10 22:34:46 +0100

    Added Player Movement + Animations
    Using PlayerController script I added Player Movement, player can now jump and move left or right.

--- Platformer/Assets/PlayerMovement.cs
+++ Platformer/Assets/PlayerMovement.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float moveSpeed = 5.0f;   // Speed of horizontal movement
+    public float jumpForce = 10.0f;  // Force applied for jumping
+    private Animator anim;
+    private Rigidbody2D Player;
+
     void Start()
     {
-        
+        Player = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        // Get user input for movement
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        Vector2 movement = new Vector2(horizontalInput * moveSpeed, Player.velocity.y);
+        Player.velocity = movement;
+
+        // Check for jumping
+        if (Input.GetButtonDown("Jump"))
+        {
+            Player.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+
+        UpdateAnimation();
+        
+    }
+    void UpdateAnimation()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (horizontalInput > 0f) 
+        {
+            anim.SetBool("running", true);
+        }
+        else if (horizontalInput < 0f)
+        {
+            anim.SetBool("running", true);
+        }
+        else 
+        {
+            anim.SetBool("running", false);
+        }
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("jumping", true);
+        }
+        else if (horizontalInput > 0f) 
+        {
+            anim.SetBool("jumping", false);
+        }
+
         
     }
 }
