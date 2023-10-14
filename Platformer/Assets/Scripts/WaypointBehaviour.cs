feb40cb83112ac531674b90d8caded5e67ddde86
YCherk <117645287+YCherk@users.noreply.github.com>
2023-10-14 16:04:37 +0100

    Added Moving Platforms

---
+++ Platformer/Assets/Scripts/WaypointBehaviour.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaformBehaviour : MonoBehaviour
+{
+    [SerializeField] private GameObject[] wp;
+    private int CurrentWPIndex = 0;
+    [SerializeField] private float Speed = 2.0f;
+
+
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(wp[CurrentWPIndex].transform.position, transform.position) < .1f)
+        {
+            CurrentWPIndex++;
+            if(CurrentWPIndex >= wp.Length)
+            {
+                CurrentWPIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, wp[CurrentWPIndex].transform.position, Time.deltaTime * Speed);
+    }
+}
