818cf9e0c11d3102dedb83e6085a46c8dea6845a
YCherk <117645287+YCherk@users.noreply.github.com>
2023-11-05 19:22:35 +0000

    updated Enemy animations.

--- Platformer/Assets/Scripts/EnemyAI.cs
+++ Platformer/Assets/Scripts/EnemyAI.cs
@@ -4,51 +4,77 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    public float moveSpeed = 2f;  // Speed of random left-right movement
-    public float followSpeed = 4f;  // Speed when following the player
-    public float followRange = 5f;  // Range within which the enemy starts following the player
-    public Transform player;  // Reference to the player's transform
-    public bool isFollowingPlayer = false;  // Flag to track if the enemy is following the player
+    public float moveSpeed = 2f;
+    public float followSpeed = 4f;
+    public float followRange = 5f;
+    public Transform player;
+    public bool isFollowingPlayer = false;
 
-    private Vector3 initialPosition;  // Initial position for random movement
-    private Vector3 targetPosition;  // Target position for random movement
+    private Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Vector3 initialPosition;
+    private Vector3 targetPosition;
 
     void Start()
     {
-        // Store the initial position for random movement
         initialPosition = transform.position;
-
-        // Set the initial random target position
         SetRandomTarget();
+
+        // Get the Animator component
+        animator = GetComponent <Animator>();
+        // Get the SpriteRenderer component
+        spriteRenderer = GetComponent <SpriteRenderer>();
     }
 
     void Update()
     {
         if (isFollowingPlayer)
         {
-            // Calculate direction to the player
             Vector3 directionToPlayer = (player.position - transform.position).normalized;
-
-            // Move towards the player
             transform.Translate(directionToPlayer * followSpeed * Time.deltaTime);
-        }
-        else
-        {
-            // Check if the player is within followRange
-            if (Vector3.Distance(transform.position, player.position) < followRange)
+
+            // Set Animator parameter for running
+            animator.SetBool("IsRunning", true);
+
+            // Flip the sprite if moving left
+            if (directionToPlayer.x < 0)
             {
-                isFollowingPlayer = true;
+                spriteRenderer.flipX = true;
             }
             else
             {
-                // Move randomly left and right within a limited range
+                spriteRenderer.flipX = false;
+            }
+        }
+        else
+        {
+            if (Vector3.Distance(transform.position, player.position) < followRange)
+            {
+                isFollowingPlayer = true;
+
+                // Set Animator parameter for running
+                animator.SetBool("IsRunning", true);
+            }
+            else
+            {
                 transform.Translate((targetPosition - transform.position).normalized * moveSpeed * Time.deltaTime);
 
-                // Check if the enemy has reached the target position
                 if (Vector3.Distance(transform.position, targetPosition) < 0.1f)
                 {
-                    // Set a new random target position
                     SetRandomTarget();
+                }
+
+                // Set Animator parameter for running
+                animator.SetBool("IsRunning", true);
+
+                // Flip the sprite if moving left
+                if (moveSpeed < 0)
+                {
+                    spriteRenderer.flipX = true;
+                }
+                else
+                {
+                    spriteRenderer.flipX = false;
                 }
             }
         }
@@ -56,7 +82,6 @@ public class EnemyAI : MonoBehaviour
 
     void SetRandomTarget()
     {
-        // Generate a random target position within the initial range
         targetPosition = initialPosition + new Vector3(Random.Range(-2f, 2f), 0, 0);
     }
 }
